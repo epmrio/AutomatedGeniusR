365e6c2275233f00f4ee8f41ccecec8d301ce7e0
epmrio <emile.provendier@gmail.com>
2020-04-04 23:13:41 +0200

    Trying to make function works

--- R/scrap_songs_artists.R
+++ R/scrap_songs_artists.R
@@ -45,16 +45,16 @@ scrap_songs_artists <- function(x) {
     print(1)
     try(songs_artistes<-get_artist_songs(id))
     print(2)
-    songs_artistes$nombre_songs_by_artist<-nrow(songs_artistes)
+    try(songs_artistes$nombre_songs_by_artist<-nrow(songs_artistes))
     print(3)
-    songs_total<-rbind(songs_total,songs_artistes)
+    try(songs_total<-rbind(songs_total,songs_artistes))
     print(4)
     #on rempli l'id_annot_total
-    songs_artistes$annotation_count_artist<-sum(songs_artistes$annotation_count)
+    try(songs_artistes$annotation_count_artist<-sum(songs_artistes$annotation_count))
     print(5)
-    id_annot_brouillon<-songs_artistes[,c("artist_id","annotation_count_artist","nombre_songs_by_artist")]
+    try(id_annot_brouillon<-songs_artistes[,c("artist_id","annotation_count_artist","nombre_songs_by_artist")])
     print(6)
-    id_annot_total<-rbind(id_annot_total,id_annot_brouillon)
+    try(id_annot_total<-rbind(id_annot_total,id_annot_brouillon))
     print(7)
     len_id<-len_id-1
   }
